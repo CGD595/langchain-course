172031573a2c5d72d7da8868ee70cae99801c7e7
Chime Gyeltshen Dorji <chimegyeltshendorji2004@gmail.com>
2025-09-06 03:55:37 +0600

    main.py update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
+
 
 def main():
     information = """
@@ -25,12 +26,14 @@ def main():
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
+    
     # llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
     llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-1.5-flash")
     # llm = ChatOllama(temperature=0, model="llama3.1:8b")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
+
 
 if __name__ == "__main__":
     main()
