30c21b81b734ab083bacb5c14d04ec3ae19e452e
Chime Gyeltshen Dorji <chimegyeltshendorji2004@gmail.com>
2025-09-06 04:15:56 +0600

    ini

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
